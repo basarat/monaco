4fcaeb8665680634b9b6d579f5791fa76f94d2ea
Basarat Syed <basaratali@gmail.com>
2016-06-10 16:29:47 +1000

    better loader def

--- src/vs/loader.d.ts
+++ src/vs/loader.d.ts
@@ -1,1 +1,11 @@
-declare var require: <T>(moduleId: [string]) => T;
+/**
+ * BAS This definition was copied from `declares.d.ts`
+ */
+declare var require: {
+	toUrl(path: string): string;
+	(moduleName: string): any;
+	(dependencies: string[], callback: (...args: any[]) => any, errorback?: (err: any) => void): any;
+	config(data: any): any;
+	onError: Function;
+	__$__nodeRequire<T>(moduleName: string): T;
+};
