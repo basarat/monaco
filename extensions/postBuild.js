c891bb96af72e718cc326ee578c893efa6c5e5ae
basarat <basaratali@gmail.com>
2016-06-11 14:32:00 +1000

    2016-06-11 [ci skip] Version: 0.201606111432.0+549478a00488e27b9ce2b754fed8ae6dc1d90bbf

--- extensions/postBuild.js
+++ extensions/postBuild.js
@@ -1,8 +1,9 @@
 "use strict";
 var utils = require("./utils");
-var srcDir = require('path').resolve(__dirname + '/../src');
-var buildDir = require('path').resolve(__dirname + '/../build');
-var allFiles = require('glob').sync('./**/*', { cwd: srcDir });
+var path = require('path');
+var srcDir = path.resolve(__dirname + '/../src');
+var buildDir = path.resolve(__dirname + '/../build');
+var allFiles = require('glob').sync('./**/*', { cwd: srcDir }).map(function (p) { return path.resolve(srcDir, p); });
 allFiles.filter(function (f) { return f.endsWith('.css'); }).forEach(function (srcCss) {
     utils.copy(srcCss, srcCss.replace(srcDir, buildDir));
 });
--- extensions/postBuild.ts
+++ extensions/postBuild.ts
@@ -3,14 +3,14 @@
  */
 /** Imports */
 import * as utils from "./utils";
-
+const path = require('path');
 /** The src dir */
-const srcDir = require('path').resolve(__dirname + '/../src');
-const buildDir = require('path').resolve(__dirname + '/../build');
+const srcDir = path.resolve(__dirname + '/../src');
+const buildDir = path.resolve(__dirname + '/../build');
 /** All the files in `src` */
-const allFiles: string[] = require('glob').sync('./**/*', { cwd: srcDir });
+const allFiles: string[] = require('glob').sync('./**/*', { cwd: srcDir }).map(p=>path.resolve(srcDir,p));
 
 /** Move any `.css` files in source to build */
-allFiles.filter(f=>f.endsWith('.css')).forEach(srcCss => {
-    utils.copy(srcCss, srcCss.replace(srcDir,buildDir));
+allFiles.filter(f => f.endsWith('.css')).forEach(srcCss => {
+    utils.copy(srcCss, srcCss.replace(srcDir, buildDir));
 })
